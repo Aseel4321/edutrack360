5f38c584300af2834c157775fba45510e562056b
Aseel4321 <aseelghaleb2088@gmail.com>
2026-03-03 07:13:40 +0300

    eث

--- src/app/core/features/auth/forgot-password/forgot-password.component.ts
+++ src/app/core/features/auth/forgot-password/forgot-password.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { 
-arrowBackOutline, mailOpenOutline,mailOutline 
-} from 'ionicons/icons';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
-arrowBackOutline = arrowBackOutline; mailOpenOutline= mailOpenOutline;mailOutline=mailOutline;
-  constructor() { }
+
+  forgotForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.forgotForm = this.fb.group({
+      email: ['', [Validators.required]]
+    });
   }
 
+  submit() {
+    if (this.forgotForm.invalid) {
+      this.forgotForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.forgotForm.value);
+    // هنا تضع API إرسال الإيميل
+  }
 }
